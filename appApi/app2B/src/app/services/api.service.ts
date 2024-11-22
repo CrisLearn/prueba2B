f7725820042b2a1987ee60e2ae66b11ca45b897e
CristianUsiña <158125455+CrisLearn@users.noreply.github.com>
2024-11-22 14:30:58 -0500

    cambio

--- appApi/app2B/src/app/services/api.service.ts
+++ appApi/app2B/src/app/services/api.service.ts
@@ -1,9 +1,13 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
-
-  constructor() { }
+  private baseUrl = 'https://dog.ceo/api/breed/Affenpinsher/images/random';
+  constructor(private http: HttpClient) { 
+    getPokemon(name: string): Observable<any> {
+      return this.http.get<any>(`${this.baseUrl}${name}`);
+  }
 }
