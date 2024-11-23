51dac0a306ca00ee63beafbd1561a36cde1611aa
CristianUsiña <158125455+CrisLearn@users.noreply.github.com>
2024-11-22 20:51:21 -0500

    servicio para el consumo de la api

--- appApi/pruebaApi/src/app/services/api.service.ts
+++ appApi/pruebaApi/src/app/services/api.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  constructor() { }
+  private robohashUrl = 'https://robohash.org/%22aqui';
+  private dogUrl = 'https://dog.ceo/api/breed/affenpinscher/images/random';
+  private gutendexUrl = 'https://gutendex.com/books?ids=1,2,3,4,5,6,7,8,9,10';
+
+  constructor(private http: HttpClient) { }
+
+  getRobohash(): Observable<any> {
+    return this.http.get(this.robohashUrl);
+  }
+
+  getDogImage(): Observable<any> {
+    return this.http.get(this.dogUrl);
+  }
+
+  getBooks(): Observable<any> {
+    return this.http.get(this.gutendexUrl);
+  }
 }
