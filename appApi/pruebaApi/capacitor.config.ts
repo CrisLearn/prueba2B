279788eda3bb15bc65dda1d1c2e5d479c7292dbc
CristianUsiña <158125455+CrisLearn@users.noreply.github.com>
2024-11-22 22:12:55 -0500

    correcion

--- appApi/pruebaApi/capacitor.config.ts
+++ appApi/pruebaApi/capacitor.config.ts
@@ -3,7 +3,17 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'pruebaApi',
-  webDir: 'www'
+  webDir: 'www',
+  bundledWebRuntime: false,
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 3000,
+      launchAutoHide: true,
+      androidScaleType: 'CENTER_CROP',
+      splashFullScreen: true,
+      splashImmersive: true
+    }
+  }
 };
 
 export default config;
--- appApi/pruebaApi/src/app/app.module.ts
+++ appApi/pruebaApi/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
+
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { HttpClientModule } from '@angular/common/http';
 import { AngularFireModule } from '@angular/fire/compat';
@@ -10,17 +11,6 @@ import { environment } from '../environments/environment';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
-// Add this constant for Firebase config
-const firebaseConfig = {
-  apiKey: "AIzaSyAhoofBl1c9nmH5wa0dcQSIPPuwfumQtdI",
-  authDomain: "b-2bff7.firebaseapp.com",
-  projectId: "b-2bff7",
-  storageBucket: "b-2bff7.appspot.com",
-  messagingSenderId: "1045510192311",
-  appId: "1:1045510192311:web:d5200b57294dbb7fe0e132",
-  measurementId: "G-M6LJ6CL8DY"
-};
-
 @NgModule({
   declarations: [AppComponent],
   imports: [
@@ -28,7 +18,7 @@ const firebaseConfig = {
     IonicModule.forRoot(),
     AppRoutingModule,
     HttpClientModule,
-    AngularFireModule.initializeApp(firebaseConfig), // Use the constant directly
+    AngularFireModule.initializeApp(environment.firebase),
     AngularFirestoreModule
   ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
