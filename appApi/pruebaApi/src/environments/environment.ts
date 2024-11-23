47f9961aee80f46ae909c229452d51cda669e781
CristianUsiña <158125455+CrisLearn@users.noreply.github.com>
2024-11-22 20:43:39 -0500

    Dependencias

--- appApi/pruebaApi/src/environments/environment.ts
+++ appApi/pruebaApi/src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAhoofBl1c9nmH5wa0dcQSIPPuwfumQtdI",
+    authDomain: "b-2bff7.firebaseapp.com",
+    projectId: "b-2bff7",
+    storageBucket: "b-2bff7.firebasestorage.app",
+    messagingSenderId: "1045510192311",
+    appId: "1:1045510192311:web:d5200b57294dbb7fe0e132",
+    measurementId: "G-M6LJ6CL8DY"
+  },
 };
 
 /*
