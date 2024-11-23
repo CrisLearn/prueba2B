06ea66e6fe8e97bf7d3d3243e8619439bbd5756b
CristianUsiña <158125455+CrisLearn@users.noreply.github.com>
2024-11-22 20:45:52 -0500

    creacion del servicio y la pagina de consumo de Api

---
+++ appApi/pruebaApi/src/app/pages/consumo-api/consumo-api.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-consumo-api',
+  templateUrl: './consumo-api.page.html',
+  styleUrls: ['./consumo-api.page.scss'],
+})
+export class ConsumoApiPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
