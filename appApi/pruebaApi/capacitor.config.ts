c5f649f6b9280b93bcf4f128df05970afa33e493
CristianUsiña <158125455+CrisLearn@users.noreply.github.com>
2024-11-25 14:01:38 -0500

    slpash

--- appApi/pruebaApi/capacitor.config.ts
+++ appApi/pruebaApi/capacitor.config.ts
@@ -9,11 +9,20 @@ const config: CapacitorConfig = {
     SplashScreen: {
       launchShowDuration: 3000,
       launchAutoHide: true,
-      androidScaleType: 'CENTER_CROP',
+      launchFadeOutDuration: 3000,
+      backgroundColor: "#ffffffff",
+      androidSplashResourceName: "splash",
+      androidScaleType: "CENTER_CROP",
+      showSpinner: true,
+      androidSpinnerStyle: "large",
+      iosSpinnerStyle: "small",
+      spinnerColor: "#999999",
       splashFullScreen: true,
-      splashImmersive: true
+      splashImmersive: true,
+      layoutName: "launch_screen",
+      useDialog: true,
     }
-  }
+  }  
 };
 
 export default config;
